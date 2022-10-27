c1b4fe066b5c6b38a2674a9ad47a1969f2884129
ginffvhjl <95149803+ginffvhjl@users.noreply.github.com>
2022-10-27 11:42:47 +0200

    Add rest of the code

--- 06.py
+++ 06.py
@@ -6,7 +6,7 @@ from hashlib import sha256
 class Block:
 
     def __init__(self, data: str, previous_hash: str):
-        self.data = data
+        self.transaction = data
         self.previous_hash = previous_hash
         self.timestamp: str = str(time.time())
         self.nonce: str = ""
@@ -14,14 +14,14 @@ class Block:
 
     @property
     def hash(self):
-        txt = self.data + self.nonce + self.previous_hash + self.timestamp
+        txt = self.transaction + self.nonce + self.previous_hash + self.timestamp
         return str(sha256(txt.encode('utf-8')).hexdigest())
 
-    def to_JSON(self):
+    def __str__(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
     def get_hashable(self, init):
-        txt = self.data + str(init) + self.nonce + self.previous_hash + self.timestamp
+        txt = self.transaction + str(init) + self.nonce + self.previous_hash + self.timestamp
         return txt.encode('utf-8')
 
     def mine_block(self, dif: int = 4):
@@ -37,7 +37,7 @@ class Block:
         return self
 
     def change_block(self):
-        self.data = "Changed string."
+        self.transaction = "Changed string."
         return self
 
 
@@ -45,6 +45,28 @@ class Wallet:
 
     def __init__(self, name):
         self.name = name
+        self.UTXOs = []
+
+    def __str__(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+    def send_funds(self, recipient, value):
+        bill = value
+        neededUTXOs = []
+
+        for utxo in self.UTXOs:
+            if bill > 0:
+                bill -= utxo.UTXO
+                neededUTXOs.append(utxo)
+        transaction = Transaction(self.name, recipient.name, value, neededUTXOs)
+
+        for UTXO in neededUTXOs:
+            self.UTXOs.remove(UTXO)
+
+        self.UTXOs.append(TransactionInput(transaction.outputs[0]))
+        recipient.UTXOs.append(TransactionInput(transaction.outputs[1]))
+
+        return transaction
 
 
 class Transaction:
@@ -54,99 +76,87 @@ class Transaction:
         self.recipient = recipient
         self.value = value
         self.inputs = inputs
+        self.transaction_id = self.calculate_hash
         self.outputs = []
-        self.transaction_id = self.calculate_hash()
+        self.outputs = self.process_transaction()
 
+    def __str__(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+    @property
     def calculate_hash(self):
         txt = self.sender + self.recipient
         return str(sha256(txt.encode('utf-8')).hexdigest())
+
+    def process_transaction(self):
+        coins_in_input = 0
+        for i in self.inputs:
+            coins_in_input += i.UTXO
+            if int(coins_in_input) < int(self.value):
+                raise ValueError("Insufficient funds!")
+        cashback = coins_in_input - int(self.value)
+        outputs = [TransactionOutput(self.sender, cashback, self.transaction_id),
+                   TransactionOutput(self.recipient, self.value, self.transaction_id)]
+        return outputs
 
 
 class TransactionInput:
 
     def __init__(self, transaction_outputs: str):
-        self.transaction_outputs = transaction_outputs
-        self.utxo = []
+        self.UTXO = transaction_outputs.value
+        self.transaction_output_id = transaction_outputs.id
 
 
 class TransactionOutput:
 
-    def __init__(self, recipient: str, value: float, parent_transaction: str):
+    def __init__(self, recipient: str, value: float, parent_transaction_id: str):
         self.recipient = recipient
-        self.value: str = str(value)
-        self.parent_transaction = parent_transaction
-        self.id = self.id
+        self.value = value
+        self.parent_transaction_id = parent_transaction_id
+        self.id = self.calculate_hash
+
+    def __str__(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
     @property
-    def id(self):
-        txt = self.recipient + self.value + self.parent_transaction
+    def calculate_hash(self):
+        txt = self.recipient + str(self.value) + self.parent_transaction_id
         return str(sha256(txt.encode('utf-8')).hexdigest())
 
 
 if __name__ == '__main__':
 
-    try:
-        difficulty = int(input("Choose difficulty (integer): "))
-    except ValueError:
-        pass
-    chain = []
-    print("\n")
+    walletC = Wallet("Julie")
+    walletD = Wallet("Robin")
+    walletE = Wallet("Zlatik")
+    DefTransaction = Transaction("0", "Julie", 300, [TransactionInput(TransactionOutput("0", 300, "0"))])
+    Def2Transaction = Transaction("0", "SAMO", 150, [TransactionInput(TransactionOutput("0", 150, "0"))])
+    Def3Transaction = Transaction("0", "Hanka", 20, [TransactionInput(TransactionOutput("0", 20, "0"))])
+    print("DefTransaction:")
+    print(DefTransaction)
+    print("Def2Transaction:")
+    print(Def2Transaction)
+    print("walletC:")
+    print(walletC)
+    print("walletD:")
+    print(walletD)
+    print("walletE:")
+    print(walletE)
 
-    chain.append(Block("Hello, I am the first block", "0"))
-    print("Mining fist block...")
-    try:
-        chain[0].mine_block(difficulty)
-    except NameError:
-        chain[0].mine_block()
-    print("First block mined: " + chain[0].current_hash)
-    # print(Block.to_JSON(genesisBlock))
-    # print("1st block's hash: " + genesisBlock.current_hash)
+    walletC.UTXOs = [TransactionInput(DefTransaction.outputs[1])]
+    firstTransaction = walletC.send_funds(walletD, 80)
+    print("firstTransaction:")
+    print(firstTransaction)
 
-    chain.append(Block("I am the second block.", chain[0].current_hash))
-    print("\nMining second block...")
-    try:
-        chain[1].mine_block(difficulty)
-    except NameError:
-        chain[1].mine_block()
-    print("Second block mined: " + chain[1].current_hash)
-    # print(Block.to_JSON(secondBlock))
-    # print("2nd block's hash: " + secondBlock.current_hash)
+    walletD.UTXOs = [TransactionInput(Def2Transaction.outputs[1])]
+    secondTransaction = walletD.send_funds(walletC, 100)
+    print("secondTransaction:")
+    print(secondTransaction)
 
-    chain.append(Block("And I am the third one.", chain[1].current_hash))
-    print("\nMining third block...")
-    try:
-        chain[2].mine_block(difficulty)
-    except NameError:
-        chain[2].mine_block()
-    print("Third block mined: " + chain[2].current_hash)
-    # print(Block.to_JSON(thirdBlock))
-    # print("3rd block's hash: " + thirdBlock.current_hash)
+    walletE.UTXOs = [TransactionInput(Def3Transaction.outputs[1])]
+    thirdTransaction = walletE.send_funds(walletC, 10)
+    print("thirdTransaction:")
+    print(thirdTransaction)
 
-    def is_chain_valid() -> bool:
-        for block in chain:
-            txt = block.data + block.nonce + block.previous_hash + block.timestamp
-            if str(sha256(txt.encode('utf-8')).hexdigest()) != block.current_hash:
-                block.current_hash = block.hash
-                return False
-        return True
-
-    changing = input("\nDo you want to change second block? (y/n): ")
-    if changing == "y":
-        print("Printing current state for control:\n" + chain[1].to_JSON())
-        chain[1].change_block()
-        print("Block changed.")
-    else:
-        print("Not changing.")
-
-    print("\nBlockchain valid: " + str(is_chain_valid()))
-
-    print("\nBlockchain: " + chain[0].to_JSON() + "\n" + chain[1].to_JSON() + "\n"
-          + chain[2].to_JSON())
-
-    walletA = Wallet("Alice")
-    walletB = Wallet("Bob")
-
-    print(walletA.name)
-
-    transaction = Transaction(walletA.name, walletB.name, 5, None)
+    print()
 
